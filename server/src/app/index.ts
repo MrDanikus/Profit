7bcf2820c8c6c517989f54a85155b68db8ef2727
Daniil <ddan62626@gmail.com>
2021-04-24 19:38:03 +0300

    feat: add security and logger middleware

--- server/src/app/index.ts
+++ server/src/app/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 
+import defaultMiddleware from '../middleware/default';
 const app = express();
 
 app.set('trust proxy', 1);
+app.use(defaultMiddleware);
 
 export default app;
