5d52c8d7dfb2e4b1b10fc7636c07aad6e1a60710
Daniil <ddan62626@gmail.com>
2021-04-28 13:26:28 +0300

    feat: util.toDotNotation
    util.toDotNotation converts js objects to dot notation e.g. {a: 1, b: {c: 2}} -> {'a': 1, 'b.c': 2}

--- server/src/utils/util.ts
+++ server/src/utils/util.ts
@@ -6,3 +6,29 @@ export function staticImplements<T>(): <U extends T>(constructor: U) => void {
     constructor;
   };
 }
+/**
+ * Converts object to dot notation
+ *
+ * @example toDotNotation({a: {b: 0}, c: 0}) -> {'a.b': 0, 'c': 0}
+ * @param obj object to be converted
+ *
+ * @return dot notation
+ */
+export function toDotNotation(
+  obj: Record<string, unknown>
+): Record<string, unknown> {
+  const res: {[key: string]: unknown} = {};
+  (function recurse(obj, current: string) {
+    // eslint-disable-next-line guard-for-in
+    for (const key in obj) {
+      const value: unknown = obj[key];
+      const newKey = current ? current + '.' + key : key;
+      if (value && typeof value === 'object' && !Array.isArray(value)) {
+        recurse(value as Record<string, unknown>, newKey);
+      } else {
+        res[newKey] = value;
+      }
+    }
+  })(obj, '');
+  return res;
+}
