0f374cca6aee441e17cdd3e5bc63ffae3b14292f
Daniil <ddan62626@gmail.com>
2021-05-03 14:19:09 +0300

    feat: vendors controller

---
+++ server/src/controllers/vendors/index.ts
@@ -0,0 +1,24 @@
+import {Request, Response, NextFunction} from 'express';
+
+import {Users} from '../../models/users';
+import {Role} from '../../models/users/role';
+
+export class VendorController {
+  /**
+   * All vendors
+   */
+  static async getAll(_req: Request, res: Response, next: NextFunction) {
+    try {
+      const vendors = await Users.find(
+        {role: Role.VENDOR},
+        '_id role name'
+      ).lean();
+      res.json({
+        count: vendors.length,
+        data: vendors,
+      });
+    } catch (err) {
+      return next(err);
+    }
+  }
+}
